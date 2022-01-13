c8eb728fa101fca3c862dc5f66f83f8e6659c40f
alfitahmad43 <alfit.ahmad13@gmail.com>
2022-01-13 17:35:43 +0700

    KonversiMataUang

---
+++ KonversiMataUang/app/src/main/java/com/alfit/konversimatauang/networking/ApiEndpoint.kt
@@ -0,0 +1,5 @@
+package com.alfit.konversimatauang.networking
+
+object ApiEndpoint {
+    var BASEURL = "https://api-exchange-rates.herokuapp.com/calculator?from={currCodefrom}&to={currCodeto}&amount={amount}"
+}
